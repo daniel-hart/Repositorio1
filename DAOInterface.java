a25ddb8d5523b4d9de163f454a8f9693260b0449
daniel-hart <daniel@hart.net>
2015-02-23 22:19:56 -0300

    primeira modificação

--- DAOInterface.java
+++ DAOInterface.java
@@ -4,7 +4,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface DAOInterface {
-	public ArrayList<String> getCellList() throws SQLException;
-	public boolean existCell(String cellName) throws SQLException;
+
+	 public ArrayList<String> getCellList() throws SQLException;
+	 public boolean existCell(String cellName) throws SQLException;
 
 }
